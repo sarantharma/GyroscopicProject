f426f64e1bc3b6748eb8121becdd5fb956965fce
CormanClan <46726812+CormanClan@users.noreply.github.com>
2022-02-07 22:14:10 -0500

    fixed missing event on moved comments
    also cleaned up some debug logs I left from last push.

--- app.js
+++ app.js
@@ -69,12 +69,10 @@ io.on("connection", (socket) => {
 
   //Editing Comment
   socket.on("commentEdit", async (cmt, commentID) => {
-	console.log(cmt +""+ commentID);
     Comment.findOneAndUpdate({_id: commentID}, {$set: {content: cmt} }, function(error, result){
    	if(error){
    		console.log("Error Editing Comment");
    	}else{
-		console.log("Yes");
    		io.emit("commentEdit", cmt, commentID);
    	}
    },);
